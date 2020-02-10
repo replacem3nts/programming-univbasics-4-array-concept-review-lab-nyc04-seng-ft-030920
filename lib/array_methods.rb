683ad33005e1b45b43f0b0fbc8e85e1e1e775ee8
Donny Landis <donny.landis@gmail.com>
2020-02-10 21:08:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,18 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  var n = 0
+  place_value = nil
+  while n < array.length; n++ {
+    if value_to_find == array[n]
+      place_value = n
+    end
+  print place_value
+  }
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max()
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min()
 end
